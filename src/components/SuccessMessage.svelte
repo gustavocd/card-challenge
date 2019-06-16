a4b3ec3c21ad8e206178085ffe93edddc1ffb3b1
Gustavo Castillo <gcdcoder@gmail.com>
2019-06-15 23:51:19 -0500

    Create SuccessMessage component

---
+++ src/components/SuccessMessage.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+<style>
+  .message {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+  }
+
+  .image {
+    height: 100px;
+  }
+
+  .copy {
+    color: #6ac259;
+    font-size: 26px;
+    text-align: center;
+  }
+</style>
+
+<div class="message" in:fade>
+  <img class="image" src="./success.svg" alt="Success message" />
+  <p class="copy">El método de pago se agregó exitosamente</p>
+</div>
